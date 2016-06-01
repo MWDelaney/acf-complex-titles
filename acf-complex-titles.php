baad666786366fd093eb3a081626c25e5d499b9c
Michael W. Delaney <michael.delaney@gmail.com>
2016-06-01 17:22:49 -0400

    Update acf-complex-titles.php

--- acf-complex-titles.php
+++ acf-complex-titles.php
@@ -199,16 +199,13 @@ License: MIT
             function replace_title($title) {
                 global $post;
                 if( have_rows('build_title') && in_the_loop() ) {
-                    
                     // Check whether the current post's title is the same as what's being passed to the filter. This prevents attempting to run the filter on content it won't work with.
                     if( $post->post_title == $title ) {
-                        $title = do_shortcode('[acfct-title]');
-                        return $title;
-                    } else {
-                        return $title;
+                        $complex_title = do_shortcode('[acfct-title]');
+                        $title = $complex_title;
                     }
-                    
-                } 
+                }
+                return $title; 
             }
 
     }
