f36e2cb969461d8f6528a6564f2b452459c89c9b
Michael W. Delaney <michael.delaney@gmail.com>
2016-12-31 01:19:43 -0500

    Update setup.php

--- lib/setup.php
+++ lib/setup.php
@@ -4,119 +4,56 @@ namespace MWD\ACF\ComplexTitles;
 
 class Setup {
 
+	/**
+	 * Variable holding the ACF fields definition to be automatically created
+	 */
+	private static $args = array (
+			'key' => 'acf_complex_titles',
+			'title' => 'Complex Titles',
+			'fields' => array (
+					array (
+							'key' => 'acfct_placeholder',
+							'label' => 'Preview Placeholder',
+							'type' => 'message',
+					),
+					array (
+							'key' => 'acfct_build_title',
+							'label' => 'Build Title',
+							'name' => 'build_title',
+							'type' => 'repeater',
+							'layout' => 'block',
+							'button_label' => 'Add Group',
+							'sub_fields' => array (
+									array (
+											'key' => 'acfct_tab_title',
+											'label' => 'Title',
+											'type' => 'tab',
+									),
+									array (
+											'key' => 'acfct_title_group',
+											'label' => 'Title Group',
+											'name' => 'title',
+											'type' => 'repeater',
+											'layout' => 'table',
+											'button_label' => 'Add Word',
+											'sub_fields' => array (),
+									),
+									array (
+											'key' => 'acfct_tab_layout',
+											'label' => 'Layout',
+											'type' => 'tab',
+									),
+							),
+					),
+			),
+		'location' => array (),
+			'menu_order' => 0,
+			'position' => 'acf_after_title',
+	);
+
 
 	public static function create_titles() {
 		if( function_exists('acf_add_local_field_group') ):
-
-		/**
-		 * Variable holding the ACF fields definition to be automatically created
-		 */
-		$args = array (
-				'key' => 'group_567332edc8e48',
-				'title' => 'Complex Titles',
-				'fields' => array (
-						array (
-								'key' => 'field_5673328771366',
-								'label' => 'Preview Placeholder',
-								'name' => '',
-								'type' => 'message',
-								'instructions' => '',
-								'required' => 0,
-								'conditional_logic' => 0,
-								'wrapper' => array (
-										'width' => '',
-										'class' => '',
-										'id' => '',
-								),
-								'message' => '',
-								'esc_html' => 0,
-								'new_lines' => 'wpautop',
-						),
-						array (
-								'key' => 'field_5673330824dd5',
-								'label' => 'Build Title',
-								'name' => 'build_title',
-								'type' => 'repeater',
-								'instructions' => '',
-								'required' => 0,
-								'conditional_logic' => 0,
-								'wrapper' => array (
-										'width' => '',
-										'class' => '',
-										'id' => '',
-								),
-								'collapsed' => '',
-								'min' => '',
-								'max' => '',
-								'layout' => 'block',
-								'button_label' => 'Add Group',
-								'sub_fields' => array (
-										array (
-												'key' => 'field_56f07d9857697',
-												'label' => 'Title',
-												'name' => '',
-												'type' => 'tab',
-												'instructions' => '',
-												'required' => 0,
-												'conditional_logic' => 0,
-												'wrapper' => array (
-														'width' => '',
-														'class' => '',
-														'id' => '',
-												),
-												'placement' => 'top',
-												'endpoint' => 0,
-										),
-										array (
-												'key' => 'field_56f05bf5f9da8',
-												'label' => 'Title Group',
-												'name' => 'title',
-												'type' => 'repeater',
-												'instructions' => '',
-												'required' => 0,
-												'conditional_logic' => 0,
-												'wrapper' => array (
-														'width' => '',
-														'class' => '',
-														'id' => '',
-												),
-												'collapsed' => '',
-												'min' => '',
-												'max' => '',
-												'layout' => 'table',
-												'button_label' => 'Add Word',
-												'sub_fields' => array (),
-										),
-										array (
-												'key' => 'field_56f07daf57698',
-												'label' => 'Layout',
-												'name' => '',
-												'type' => 'tab',
-												'instructions' => '',
-												'required' => 0,
-												'conditional_logic' => 0,
-												'wrapper' => array (
-														'width' => '',
-														'class' => '',
-														'id' => '',
-												),
-												'placement' => 'top',
-												'endpoint' => 0,
-										),
-								),
-						),
-				),
-			'location' => array (),
-				'menu_order' => 0,
-				'position' => 'acf_after_title',
-				'style' => 'default',
-				'label_placement' => 'top',
-				'instruction_placement' => 'label',
-				'hide_on_screen' => '',
-				'active' => 1,
-				'description' => '',
-		);
-
 
 		/**
 		* Check for declared post types to attach fields to
@@ -162,7 +99,7 @@ class Setup {
 				);
 		}
 		// Insert each location into the $args array
-		$args['location'] = $locations_enabled;
+		self::$args['location'] = $locations_enabled;
 
 
 		/**
@@ -185,24 +122,22 @@ class Setup {
 		} else {
 				// If theme support is not explicitly defined, enable all fields as a fallback.
 				$layout_fields_enabled = array();
-				foreach(get_class_methods('\MWD\ACF\ComplexTitles\Layout') as $layout_field) {
-						if($layout_field != '__construct') {
-								$layout_fields_enabled[] = $layout_field;
-						}
+				foreach(get_class_vars('\MWD\ACF\ComplexTitles\Layout') as $name => $value) {
+					$layout_fields_enabled[] = $name;
 				}
 		}
 		// Enable each layout field
 		// If no layout fields are enabled, remove the tabs to avoid clutter
 		if( count( $layout_fields_enabled ) <= 0 ) {
-				unset($args['fields'][1]['sub_fields'][2]);
-				unset($args['fields'][1]['sub_fields'][0]);
+				unset(self::$args['fields'][1]['sub_fields'][2]);
+				unset(self::$args['fields'][1]['sub_fields'][0]);
 		} else {
 
 				// Enable each layout field
 				$CTLayouts      = new \MWD\ACF\ComplexTitles\Layout();
 				$layout_fields_array  = array();
-				foreach ($layout_fields_enabled as $layout_field) {
-						$layout_fields_array[]    = $CTLayouts->$layout_field();
+				foreach ($layout_fields_enabled as $name) {
+					$layout_fields_array[] = $CTLayouts->$name;
 				}
 				// Sort layout fields by the 'order' element
 				usort($layout_fields_array, function ($a, $b) {
@@ -211,7 +146,7 @@ class Setup {
 				});
 				// Insert each layout field into the $args array
 				foreach ( $layout_fields_array as $layout_field) {
-						$args['fields'][1]['sub_fields'][] = $layout_field['field'];
+						self::$args['fields'][1]['sub_fields'][] = $layout_field['field'];
 				}
 		}
 
@@ -236,18 +171,16 @@ class Setup {
 		} else {
 				// If theme support is not explicitly defined, enable all fields as a fallback.
 				$fields_enabled = array();
-				foreach(get_class_methods('\MWD\ACF\ComplexTitles\Fields') as $field) {
-						if($field != '__construct') {
-								$fields_enabled[] = $field;
-						}
+				foreach(get_class_vars('\MWD\ACF\ComplexTitles\Fields') as $name => $value) {
+					$fields_enabled[] = $name;
 				}
 		}
 
 		// Enable each layout field
 		$CTFields      = new Fields();
 		$fields_array  = array();
-		foreach ($fields_enabled as $field) {
-				$fields_array[]    = $CTFields->$field();
+		foreach ($fields_enabled as $name) {
+				$fields_array[]	= $CTFields->$name;
 		}
 		// Sort layout fields by the 'order' element
 		usort($fields_array, function ($a, $b) {
@@ -256,7 +189,7 @@ class Setup {
 		});
 		// Insert each layout field into the $args array
 		foreach ( $fields_array as $field) {
-				$args['fields'][1]['sub_fields'][1]['sub_fields'][] = $field['field'];
+				self::$args['fields'][1]['sub_fields'][1]['sub_fields'][] = $field['field'];
 		}
 
 
@@ -269,7 +202,7 @@ class Setup {
 		*
 		*/
 
-		acf_add_local_field_group($args);
+		acf_add_local_field_group(self::$args);
 
 		endif;
 	}
