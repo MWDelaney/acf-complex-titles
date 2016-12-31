7e5fcc11bb115c2b43b9f6c068c036a57a25f251
Michael W. Delaney <michael.delaney@gmail.com>
2016-12-31 01:20:00 -0500

    Update layout.php

--- lib/layout.php
+++ lib/layout.php
@@ -4,18 +4,6 @@ namespace MWD\ACF\ComplexTitles;
 
     class Layout {
 
-		private $layout;
-		private $repeater;
-
-		function __construct($repeater = null) {
-			$this->repeater = $repeater;
-			$this->key 		= 'acfct-';
-			$this->key 	   .= (isset($repeater)) ? 'repeater-' . $repeater : null;
-        	$this->key     .= 'layoutfield-';
-		}
-
-
-
 		/**
 		 *
 		 * Layout Field: Alignment
@@ -25,39 +13,19 @@ namespace MWD\ACF\ComplexTitles;
 		 *
 		 * Select
 		 */
-		function alignment() {
-			return(
-			    array ( 'order' => '50',
-				    'field' => array (
-						'key' => $this->key . __FUNCTION__,
-				        'label' => 'Alignment',
-				        'name' => 'alignment',
-				        'type' => 'select',
-				        'instructions' => '',
-				        'required' => 0,
-				        'conditional_logic' => 0,
-				        'wrapper' => array (
-				            'width' => '',
-				            'class' => '',
-				            'id' => '',
-				        ),
-				        'choices' => array (
-				            'none' => 'None',
-				            'left' => 'Left',
-				            'right' => 'Right',
-				            'center' => 'Center',
-				        ),
-				        'default_value' => array (
-				        ),
-				        'allow_null' => 0,
-				        'multiple' => 0,
-				        'ui' => 0,
-				        'ajax' => 0,
-				        'placeholder' => '',
-				        'disabled' => 0,
-				        'readonly' => 0,
-				    )
-				)
-			);
-		}
+		public $alignment = array (
+			'order' => '50',
+			'field' => array (
+				'key' => 'acfct_layout_alignment',
+				'label' => 'Alignment',
+				'name' => 'alignment',
+				'type' => 'select',
+				'choices' => array (
+					'none' => 'None',
+					'left' => 'Left',
+					'right' => 'Right',
+					'center' => 'Center',
+				),
+			)
+		);
 }
