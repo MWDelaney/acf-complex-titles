ff53556407cdfd1cb1dc518d76e904b6e14e6bcf
Michael W. Delaney <michael.delaney@gmail.com>
2016-12-31 01:22:03 -0500

    Update fields.php

--- lib/fields.php
+++ lib/fields.php
@@ -2,17 +2,7 @@
 
 namespace MWD\ACF\ComplexTitles;
 
-  class Fields {
-
-		private $layout;
-		private $repeater;
-
-		function __construct($repeater = null) {
-			$this->repeater = $repeater;
-			$this->key 		= 'acfct-';
-			$this->key 	   .= (isset($repeater)) ? '-repeater-' . $repeater : null;
-			$this->key     .= '-field-';
-		}
+	class Fields {
 
 
 		/**
@@ -23,33 +13,15 @@ namespace MWD\ACF\ComplexTitles;
 		 *
 		 * Text
 		 */
-		function word_or_phrase() {
-			return(
-			    array ( 'order' => '0',
-			        'field' => array (
-			            'key' => 'field_5673331424dd6',
-			            'label' => 'Word or Phrase',
-			            'name' => 'word_or_phrase',
-			            'type' => 'text',
-			            'instructions' => '',
-			            'required' => 0,
-			            'conditional_logic' => 0,
-			            'wrapper' => array (
-			                'width' => '',
-			                'class' => '',
-			                'id' => '',
-			            ),
-			            'default_value' => '',
-			            'placeholder' => '',
-			            'prepend' => '',
-			            'append' => '',
-			            'maxlength' => '',
-			            'readonly' => 0,
-			            'disabled' => 0,
-			        )
-			    )
-			);
-		}
+		public $word_or_phrase = array (
+			'order' => '0',
+			'field' => array (
+				'key' => 'acfct_word_or_phrase',
+				'label' => 'Word or Phrase',
+				'name' => 'word_or_phrase',
+				'type' => 'text',
+			)
+		);
 
 
 
@@ -61,41 +33,21 @@ namespace MWD\ACF\ComplexTitles;
 		 *
 		 * Select
 		 */
-		function alignment() {
-			return(
-			    array ( 'order' => '50',
-			        'field' => array (
-			            'key' => 'field_56f056b90e0a7',
-			            'label' => 'Alignment',
-			            'name' => 'alignment',
-			            'type' => 'select',
-			            'instructions' => '',
-			            'required' => 0,
-			            'conditional_logic' => 0,
-			            'wrapper' => array (
-			                'width' => '',
-			                'class' => '',
-			                'id' => '',
-			            ),
-			            'choices' => array (
-			                'none' => 'None',
-			                'left' => 'Left',
-			                'right' => 'Right',
-			                'center' => 'Center',
-			            ),
-			            'default_value' => array (
-			            ),
-			            'allow_null' => 0,
-			            'multiple' => 0,
-			            'ui' => 0,
-			            'ajax' => 0,
-			            'placeholder' => '',
-			            'disabled' => 0,
-			            'readonly' => 0,
-			        )
-			    )
-			);
-		}
+		public $alignment = array (
+			'order' => '50',
+			'field' => array (
+				'key' => 'acfct_alignment',
+				'label' => 'Alignment',
+				'name' => 'alignment',
+				'type' => 'select',
+				'choices' => array (
+					'none' => 'None',
+					'left' => 'Left',
+					'right' => 'Right',
+					'center' => 'Center',
+				),
+			)
+		);
 
 
 
@@ -107,28 +59,15 @@ namespace MWD\ACF\ComplexTitles;
 		 *
 		 * Checkbox
 		 */
-		function emphasize() {
-			return(
-			    array ( 'order' => '10',
-			        'field' => array (
-			            'key' => 'field_5673332224dd7',
-			            'label' => 'Emphasize',
-			            'name' => 'emphasize',
-			            'type' => 'true_false',
-			            'instructions' => '',
-			            'required' => 0,
-			            'conditional_logic' => 0,
-			            'wrapper' => array (
-			                'width' => '',
-			                'class' => '',
-			                'id' => '',
-			            ),
-			            'message' => '',
-			            'default_value' => 0,
-			        )
-			    )
-			);
-		}
+		public $emphasize = array (
+			'order' => '10',
+			'field' => array (
+				'key' => 'acfct_emphasize',
+				'label' => 'Emphasize',
+				'name' => 'emphasize',
+				'type' => 'true_false',
+			)
+		);
 
 
 
@@ -140,39 +79,19 @@ namespace MWD\ACF\ComplexTitles;
 		 *
 		 * Select
 		 */
-		function size() {
-			return(
-			    array ( 'order' => '30',
-			        'field' => array (
-			            'key' => 'field_56f143ff070bc',
-			            'label' => 'Size',
-			            'name' => 'size',
-			            'type' => 'select',
-			            'instructions' => '',
-			            'required' => 0,
-			            'conditional_logic' => 0,
-			            'wrapper' => array (
-			                'width' => '',
-			                'class' => '',
-			                'id' => '',
-			            ),
-			            'choices' => array (
-			                'md' => 'Medium',
-			                'sm' => 'Small',
-			                'lg' => 'Large',
-			            ),
-			            'default_value' => array (
-			            ),
-			            'allow_null' => 0,
-			            'multiple' => 0,
-			            'ui' => 0,
-			            'ajax' => 0,
-			            'placeholder' => '',
-			            'disabled' => 0,
-			            'readonly' => 0,
-			        )
-			     )
-			);
-		}
+		public $size = array (
+			'order' => '30',
+			'field' => array (
+				'key' => 'acfct_size',
+				'label' => 'Size',
+				'name' => 'size',
+				'type' => 'select',
+				'choices' => array (
+					'md' => 'Medium',
+					'sm' => 'Small',
+					'lg' => 'Large',
+				)
+			)
+		);
 
-	}
+}
