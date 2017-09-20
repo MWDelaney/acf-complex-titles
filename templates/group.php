d1e2d19e8ad48f1b14ec43fd17acc468dfb7ce6a
Michael W. Delaney <michael.delaney@gmail.com>
2017-09-20 14:23:07 -0400

    Update group.php

--- templates/group.php
+++ templates/group.php
@@ -1,12 +1,13 @@
 
 <span class="<?=$context->classes?>">
-	<?php if( have_rows('title') ): ?>
-	<?php while ( have_rows('title') ) : the_row(); ?>
-		<?php
-			do_action('acf-complex-titles-before-element');
-			$context->template->get_template_part( 'element', get_post_type() );
-			do_action('acf-complex-titles-before-element');
+  <?php if( have_rows('title', $context->id) ): ?>
+  <?php while ( have_rows('title', $context->id) ) : the_row(); ?>
+    <?php
 
-		?>
-	<?php endwhile; endif;?>
+      do_action('acf-complex-titles-before-element', $context->id);
+      $context->template->get_template_part( 'element', get_post_type() );
+      do_action('acf-complex-titles-before-element', $context->id);
+
+    ?>
+  <?php endwhile; endif;?>
 </span>
