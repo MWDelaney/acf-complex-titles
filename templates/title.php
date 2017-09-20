3ef2386821e72e1a3e5429b519b2132862865217
Michael W. Delaney <michael.delaney@gmail.com>
2017-09-20 14:23:38 -0400

    Update title.php

--- templates/title.php
+++ templates/title.php
@@ -1,12 +1,12 @@
 
-<?php if( have_rows('build_title') ): ?>
-	<span class="complex-title">
-		<?php while ( have_rows('build_title') ) : the_row(); ?>
-			<?php
-				do_action('acf-complex-titles-before-group');
-				$context->template->get_template_part( 'group', get_post_type() );
-				do_action('acf-complex-titles-after-group');
-			?>
-		<?php endwhile; ?>
-	</span>
+<?php if( have_rows('build_title', $context->id) ): ?>
+  <span class="complex-title">
+    <?php while ( have_rows('build_title', $context->id) ) : the_row(); ?>
+      <?php
+        do_action('acf-complex-titles-before-group', $context->id);
+        $context->template->get_template_part( 'group', get_post_type() );
+        do_action('acf-complex-titles-after-group', $context->id);
+      ?>
+    <?php endwhile; ?>
+  </span>
 <?php endif; ?>
